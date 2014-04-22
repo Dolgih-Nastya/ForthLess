80591e150bc97d9942b1a7a06114fb61a12e8059
Dolgikh <dolgikh@Andreys-Mac-mini-2.local>
2014-04-22 17:26:08 +0300

    Initial Commit

---
+++ ForthLesson/UIKTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UIKTAppDelegate.h
+//  ForthLesson
+//
+//  Created by Dolgikh on 4/22/14.
+//  Copyright (c) 2014 Anastasia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIKTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ForthLesson/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ForthLesson
+//
+//  Created by Dolgikh on 4/22/14.
+//  Copyright (c) 2014 Anastasia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UIKTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UIKTAppDelegate class]));
+    }
+}
